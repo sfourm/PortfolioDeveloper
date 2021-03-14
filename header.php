d7b7378e3a1186022ee40839ec9f1985438bdf93
Samuel Sergio <69014368+sfourm@users.noreply.github.com>
2021-03-14 16:08:01 -0300

    Commit Inicial

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Samuel Sergio</title>
+        
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/style.css"> 
+
+        <!-- FONTES-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+        <!-- FONT AWESOME--> 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="MenuLateral justify-content-center">
+            <div class="MenuLateralImagem">
+                <a href="index.php"><img src="img/perfil.jpg" alt="Samuel Sergio" class="img-fluid rounded-circle ImagemPerfil"></a>
+                <h3>Samuel Sergio</h3>
+                <p>Software Engineer</p>
+            </div>
+            <nav class="MenuLateralLista">
+                <ul>
+                <li><a  class="">SERVIÇOS</a></li>
+                <li><a  class="">PRODUTOS</a></li>
+                <li><a  class="">QUEM SOMOS</a></li>
+                <li><a  class="">ORÇAMENTO</a></li>
+                <li ><a class="">CONTATO</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div class="wrapper">
+        
---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(window).scroll(function () {
+	var scrollTop = $(window).scrollTop();
+
+	if (scrollTop > 20) {
+      $('.site-navbar').css("background-color", "rgba(0, 0, 0, 1)");
+      $('.site-mobile-menu .site-menu > li > span').css("padding-top", "12px");
+		$('.site-navbar').addClass('position-fixed');
+		$('.site-navbar').addClass('menu-padding');
+		$('.site-navbar').removeClass('py-4');
+
+	} else if (scrollTop  < 20) {
+      $('.site-navbar').css("background-color", "rgba(0, 0, 0, 0)");
+      $('.site-mobile-menu .site-menu > li > span').css("padding-top", "30px");
+		$('.site-navbar').removeClass('position-fixed')
+		$('.site-navbar').removeClass('menu-padding');
+		$('.site-navbar').addClass('py-4');
+	};
+
+});
+
+(function ($) {
+   //Humberger Menu
+   $(".humberger__open").on('click', function () {
+       $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+       $(".navbar-toggler-awesome").removeClass("fa-bars");
+       $(".navbar-toggler-awesome").addClass("fa-times");
+       $(".humberger__menu__overlay").addClass("active");
+
+       $("body").addClass("over_hid");
+   });
+
+   $(".humberger__menu__overlay").on('click', function () {
+       $(".humberger__menu__wrapper").removeClass("show__humberger__menu__wrapper");
+       $(".navbar-toggler-awesome").removeClass("fa-times");
+       $(".navbar-toggler-awesome").addClass("fa-bars");
+       $(".humberger__menu__overlay").removeClass("active");
+       $("body").removeClass("over_hid");
+   });
+})(jQuery)
+
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000');
+});
